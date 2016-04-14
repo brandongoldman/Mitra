2f6bef6550a21aab3c7620c185d1f76a05f4e6fa
Brandon Goldman <brandon.m.goldman@gmail.com>
2016-04-14 15:27:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+  // On the first line you can read the number of realms, 𝑁, on the next 3 × 𝑁 lines you can read, on each first
+  // line the charm for the realm, on each second line the number of magi which Kael’zak would meet in that
+  // realm and on each third line the power of the aforementioned magi in the order that they would meet
+  // Kael’zak in the realm. On the following two lines you can read, respectively, the charm for Kael’zak’s realm
+  // and the charm for the destination realm.
+ int numOfRealms, numOfMagi, magiPower;
+ string charm;
+ 
+  return 0;
+}
