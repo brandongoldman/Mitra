472bd878c38bc1602ce7ac3c4d71b0efd279c124
gruszczykr <gruszczykr@ufl.edu>
2016-04-23 13:49:54 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
+/* ---------------------------------------------------------------------------
+** Project 2 - Mitra, The Messenger of Love
+** COP3530 - Spring 2016
+**
+** Authors: 
+** 	Brandon Goldman, Michael Riccardi, Harrison Magoutas, Ryan Gruszczyk
+**
+** -------------------------------------------------------------------------*/
+
 #include <iostream>
-//#include <unordered_map>
+#include <unordered_map>
 #include <vector>
-//#include <queue>
-//#include <algorithm>
+#include <queue>
+#include <algorithm>
 #include <string>
-//Git test - Ryan 
+
 using namespace std;
 
 struct realm
@@ -18,9 +27,48 @@ int  maxIncantations(vector <int> magi)
 	// determine most number of possible steps
 }
 
+// edit distance algorithm to find string alignment
 int minIncantationsBetween(string startingRealm, string endingRealm)
 {
 	// return minimum number of incantations needed to go start -> end
+	int sizeOfStart = startingRealm.length() + 1;
+	int sizeOfEnd = endingRealm.length() + 1;
+	int distanceBetween[sizeOfStart][sizeOfEnd];
+
+	// PSEUDOCODE FROM THE INTERWEBS WOO!!
+	// parse string
+	// for size of A (0:i)
+	// 		for size of B (0:j)
+	//			if i=0, dist[i][j] = j;
+	//			if j=0, dist[i][j] = i;
+	//			if char at A(i-1) = char at B(j-1)
+	//				dist[i][j] = dist[i-1][j-1];
+	//			else dist[i][j] = 1 + findMinOf(dist[i][j-1], dist[i-1][j], dist[i-1][j-1]);
+
+	for(int i = 0; i <= startingRealm.size(); i++)
+	{
+		for(int j = 0; j <= endingRealm.size(); j++)
+		{
+			if (i == 0)
+			{
+				distanceBetween[i][j] = j;
+			}
+			else if (j == 0)
+			{
+				distanceBetween[i][j] = i;
+			}
+			else if (startingRealm.at(i - 1) == endingRealm.at(j - 1))
+			{
+				distanceBetween[i][j] = dist[i-1][j-1];
+			}
+			else
+			{
+				distanceBetween[i][j] = 1 + min( min(distanceBetween[i][j-1], distanceBetween[i-1][j]), distanceBetween[i-1][j-1]);
+			}
+		}
+	}
+
+	return distanceBetween[ startingRealm.length() ][ endingRealm.length() ];
 }
 
 // Dijkstra's Method to determine least heavy path start -> end
@@ -47,15 +95,24 @@ int main()
 {
   int numOfRealms, numOfMagi, magiPower;
   string charm, startingCharm, destinationCharm;
+  vector <int> sequenceOfMagi;
 
   cin >> numOfRealms;
 
-  // for number of realms
-	cin >> charm;
+  for(int i = 0; i < numOfRealms; i++)
+  {
+  	cin >> charm;
   	cin >> numOfMagi;
+  	sequenceOfMagi.resize(numOfMagi);	// set size of vector to number of magi
+  	
+  	for(int j = 0; j < numOfMagi; j++)
+  	{
+  		cin >> sequenceOfMagi.at(j);
+  	}
+  }
 
-  cin >> startingCharm >> destinationCharm;
+  cin >> startingCharm;
+  cin >> destinationCharm;
 
- 
   return 0;
 }
