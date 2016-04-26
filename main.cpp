6b9f2eb822526bb5e0204d43801a266758b08500
Michael Riccardi <michaelriccardi@ufl.edu>
2016-04-26 13:30:36 -0400

    Implemented Dijkstra's algorithm
    Some test cases working, others segmentation faulting

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include <algorithm>
 #include <string>
 #include <cmath>
+#include <climits>
 
 using namespace std;
 
@@ -38,11 +39,15 @@ struct Vertex
 {
 	bool visited;
 	Realm realm;
+	int distance;
+	int cost;
 	vector<Edge*> edges;
 	Vertex(Realm r)
 	{
 		realm = r;
 		visited = false;
+		distance = INT_MAX;
+		cost = 0;
 	}
 	void addEdge(Edge* e)
 	{
@@ -53,11 +58,13 @@ struct Vertex
 struct Edge
 {
 	int weight;
+	int cost;
 	Vertex* next;
-	Edge(Vertex* n, int w)
+	Edge(Vertex* n, int w, int c)
 	{
 		weight = w;
 		next = n;
+		cost = c;
 	}
 };
 
@@ -216,20 +223,6 @@ int minIncantationsBetween(string startingRealm, string endingRealm)
 	return distanceBetween[ startingRealm.length() ][ endingRealm.length() ];
 }
 
-bool checkIfPossibleStartToFinish()
-{
-	// does # steps start to finish == # possible incantations
-	// if not, bool is IMPOSSIBLE, print statement in different method
-	
-	// Michael: this might be covered in Dijkstra's algorithm
-}
-
-// this is for Dijkstra's method to determine least heavy path from s->f
-void shortestPath(Graph graph)
-{
-	// cout incantations + " " + gems;
-}
-
 Graph makeGraph(vector<Realm> realms)
 {
 	Graph graph = Graph();
@@ -244,46 +237,200 @@ Graph makeGraph(vector<Realm> realms)
 	for(int i = 0; i < realms.size(); i++)
 	{
 		// try to link each to the realms after it
-		for(int j = i + 1; j < realms.size(); i++)
+		for(int j = 0; j < realms.size(); j++)
 		{
-			int available = maxIncantations(realms[i].magi).size(); // # of incantations possible 
+			if(i == j) { continue; }
+			//cout << "i = " << i << "; j = " << j << endl;
+			vector<int> incantations = maxIncantations(realms[i].magi);
+			
+			int available = incantations.size(); // # of incantations possible 
 			int needed = minIncantationsBetween(realms[i].charm, realms[j].charm); // # of incantations needed
 			if(available >= needed) // if it is possible to travel from realm[i] to realm[j]
 			{
+				int cost = 0;
+				for(int k = 0; k < needed; k++)
+				{
+					cost += incantations[k];
+				}
 				// add edge connecting the two, with weight of the # of incantations needed
-				graph.vertices[i]->addEdge(new Edge(graph.vertices[j], needed));
+				graph.vertices[i]->addEdge(new Edge(graph.vertices[j], needed, cost));
 			}
 		}
 	}
 	
 	return graph;
 }
-	// for return trip, reverse vector
 
-// if impossible, make num of incantations = -1 || set minIncantations = -1
+// Dijkstra's Method to determine least heavy path start -> end
+void shortestPath(Graph &graph, string startingCharm, string destinationCharm)
+{
+	int N = graph.vertices.size();
+	
+	int startIndex = -1;
+	int destinationIndex = -1;
+	bool startFound = false;
+	bool destinationFound = false;
+	
+	for(int i=0; i < graph.vertices.size(); i++)
+	{
+		if(graph.vertices[i]->realm.charm == startingCharm)
+		{
+			startIndex = i;
+			startFound = true;
+		}
+		if(graph.vertices[i]->realm.charm == destinationCharm)
+		{
+			destinationIndex = i;
+			destinationFound = true;
+		}
+		if(startFound && destinationFound)
+		{
+			break;
+		}
+	}
+	
+	if(startIndex == -1 || destinationIndex == -1)
+	{
+		cout << "IMPOSSIBLE" << endl;
+		return;
+	}
+	
+	/* 1. Assign to every node a tentative distance value: set it to zero
+	 * for our initial node and to infinity for all other nodes. */
+
+	graph.vertices[startIndex]->distance = 0;
+	// others are set to INT_MAX by default
+	
+	/* 2. Set the initial node as current. Mark all other nodes unvisited.
+	 * Create a set of all the unvisited nodes called the unvisited set. */
+	 
+	Vertex* curVertex = graph.vertices[startIndex];
+	// others are unvisited by default
+	
+	int minDistance = INT_MAX;
+	Vertex* minVertex = NULL;
+	
+	int shortestIndex = -1;
+	
+	do
+	{
+		minVertex = curVertex->edges[0]->next;
+		minDistance = curVertex->distance;
+		//cout << curVertex->realm.charm << endl;
+
+		/* 3. For the current node, consider all of its unvisited neighbors and
+		 * calculate their tentative distances. Compare the newly calculated
+		 * tentative distance to the current assigned value and assign the
+		 * smaller one. For example, if the current node A is marked with a
+		 * distance of 6, and the edge connecting it with a neighbor B has
+		 * length 2, then the distance to B (through A) will be 6 + 2 = 8.
+		 * If B was previously marked with a distance greater than 8 then
+		 * change it to 8. Otherwise, keep the current value. */
+		 
+		//cout << curVertex->edges.size() << endl;
+				
+		for(int i=0; i < curVertex->edges.size(); i++)
+		{
+			Edge* curEdge = curVertex->edges[i];
+			int curWeight = curEdge->weight;
+			int curCost = curEdge->cost;
+			Vertex* next = curEdge->next;
+			
+			if(!next->visited)
+			{				
+				//cout << "  " << next->realm.charm << endl;
+				
+				int tentative_distance = curVertex->distance + curWeight;
+				
+				if(tentative_distance < next->distance)
+				{
+					next->distance = tentative_distance;
+					next->cost = curVertex->cost + curCost;
+					//cout << "    shorter found" << endl;
+				}				
+			}			
+		}
+		
+		/* 4. When we are done considering all of the neighbors of the current node,
+		*  mark the current node as visited and remove it from the unvisited set.
+		*  A visited node will never be checked again. */
+		curVertex->visited = true;
+	
+		/* 6. Otherwise, select the unvisited node that is marked with the smallest
+		 * tentative distance, set it as the new "current node", and go back to step 3. */
+		 
+		int shortestDistance = INT_MAX;
+		shortestIndex = -1;
+		
+		for(int i=0; i < graph.vertices.size(); i++)
+		{
+			Vertex* Vi = graph.vertices[i];
+			if(!Vi->visited)
+			{
+				if(Vi->distance < shortestDistance)
+				{
+					shortestIndex = i;
+					shortestDistance = Vi->distance;
+				}
+			}
+		}
+		if(shortestIndex >= 0)
+		{
+			curVertex = graph.vertices[shortestIndex];
+		}
+	
+		/* 5. If the destination node has been marked visited (when planning a
+		 * route between two specific nodes) or if the smallest tentative distance
+		 * among the nodes in the unvisited set is infinity (when planning a complete
+		 * traversal; occurs when there is no connection between the initial node and
+		 * remaining unvisited nodes), then stop. The algorithm has finished. */
+		 
+	} while(!graph.vertices[destinationIndex]->visited && shortestIndex >= 0);
+	
+	if(graph.vertices[destinationIndex]->visited)
+	{
+		cout << graph.vertices[destinationIndex]->distance << " " << graph.vertices[destinationIndex]->cost << endl;
+	}
+	else
+	{
+		cout << "IMPOSSIBLE" << endl;
+	}
+}
 
 int main()
 {
-  int numOfRealms, numOfMagi, magiPower;
-  string charm, startingCharm, destinationCharm;
-  vector <int> sequenceOfMagi;
+	int numOfRealms, numOfMagi, magiPower;
+	string charm, startingCharm, destinationCharm;
+	vector<Realm> realms;
+	vector <int> sequenceOfMagi;
 
-  cin >> numOfRealms;
+	cin >> numOfRealms;
 
-  for(int i = 0; i < numOfRealms; i++)
-  {
-  	cin >> charm;
-  	cin >> numOfMagi;
-  	sequenceOfMagi.resize(numOfMagi);	// set size of vector to number of magi
+	for(int i = 0; i < numOfRealms; i++)
+	{
+		Realm realm = Realm();
+		cin >> realm.charm;
+		cin >> numOfMagi;
+
+		sequenceOfMagi.resize(numOfMagi); // set size of vector to number of magi
   	
-  	for(int j = 0; j < numOfMagi; j++)
-  	{
-  		cin >> sequenceOfMagi.at(j);
-  	}
-  }
+		for(int j = 0; j < numOfMagi; j++)
+		{
+			int power;
+			cin >> sequenceOfMagi[j];
+			realm.magi.push_back(power);
+		}
+		realm.magi = sequenceOfMagi;
+		realms.push_back(realm);
+	}
 
-  cin >> startingCharm;
-  cin >> destinationCharm;
+	cin >> startingCharm;
+	cin >> destinationCharm;
 
-  return 0;
+	Graph graph = makeGraph(realms);
+	shortestPath(graph, startingCharm, destinationCharm);
+	Graph graph2 = makeGraph(realms); // second graph because the method modifies the original
+	shortestPath(graph2, destinationCharm, startingCharm);
+
+	return 0;
 }
